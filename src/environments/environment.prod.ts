711a2d9cd354c45d1824dfafb2ba0cf6966430fa
theLastKing711 <lastking711@protonmail.com>
2022-07-16 04:46:52 +0300

    fix api route name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  base_url: "https://localhost:7267/api/",
-  imagesPath: "https://localhost:7267/images/"
-  // base_url: "https://e-commerce-api1.herokuapp.com/weatherforecast/api/",
-  // imagesPath: "https://e-commerce-api1.herokuapp.com/weatherforecast/images/"
+  base_url: "https://e-commerce-api1.herokuapp.com/api/",
+  imagesPath: "https://e-commerce-api1.herokuapp.com/weatherforecast/images/"
 };
