7e79c794383eed77318054818490d15ce683d934
theLastKing711 <lastking711@protonmail.com>
2022-08-07 14:41:34 +0300

    fixes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { AsideComponent } from './aside/aside.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     AsideComponent,
+    SearchComponent,
   ],
   imports: [
     CommonModule,
---
+++ src/app/core/search/search.component.ts
@@ -0,0 +1,75 @@
+import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { CategoryItemDto } from 'src/types/category';
+import { ProductItemDto } from 'src/types/product';
+import { StorageService } from 'src/app/services/storage.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { StatsService } from 'src/app/services/stats.service';
+import { AlertifyService } from 'src/app/services/alertify.service';
+import { HeaderBackDropService } from 'src/app/services/header-back-drop.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+
+
+  query!: string;
+  productsSearchList!: ProductItemDto[];
+  categorySearchList!: CategoryItemDto[];
+
+  searchSubscrption!: Subscription;
+
+  constructor(private storageService: StorageService,
+              private authService: AuthService,
+              private statsService: StatsService,
+              private alertifyService: AlertifyService,
+              private headerBackDropService: HeaderBackDropService) { }
+
+  ngOnInit(): void {
+  }
+
+  searchItems(e: any) {
+    this.headerBackDropService.activateBackDrop();
+
+    const query = e.target.value;
+
+
+    this.searchSubscrption = this.statsService.searchItems(query)
+                                              .subscribe(searchResult => {
+                                                console.log("search result", searchResult);
+                                                this.productsSearchList = [...searchResult.productListDto];
+                                                this.categorySearchList = [...searchResult.categoryListDto];
+                                              })
+  }
+
+
+  onBlur(e: any)
+  {
+    this.headerBackDropService.deActivateBackDrop();
+  }
+
+  isLoggedIn() {
+    return this.storageService.isAuthenticated();
+  }
+
+
+  getLoggedUser() {
+    return this.authService.getUser();
+  }
+
+  logout() {
+    this.alertifyService.warning("Logged out successfully")
+    this.storageService.removeFromStorage("access_token")
+  }
+
+
+  ngOnDestroy(): void {
+    if(this.searchSubscrption){
+      this.searchSubscrption.unsubscribe();
+    }
+  }
+
+}
